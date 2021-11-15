4402d6bce83adde5a34d9a773d37ff114b9cca08
Ha Tran <tranvietha.2809@gmail.com>
2021-11-15 23:00:46 +0700

    Initial Commit

---
+++ src/state/store.js
@@ -0,0 +1,7 @@
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers/index.js";
+import thunk from 'redux-thunk';
+
+export const store = createStore(
+    reducers, {}, applyMiddleware(thunk)
+)
